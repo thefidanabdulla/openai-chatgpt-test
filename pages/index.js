02dc76f58fb1a1d92843d75dd12462fa47b25f62
Galaxomas <thefidanabdulla@gmail.com>
2023-04-08 00:27:21 +0400

    chatgpt test

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userInput),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+
+      setResult(data?.result?.choices[0]?.text);
+      setUserInput("");
+      const items =  result.split('\n').filter(item => item !== '')
+      console.log(items)
+      
+    } catch(error) {
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h3>Test ChatGPT</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="input"
+            placeholder="Enter an question"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
